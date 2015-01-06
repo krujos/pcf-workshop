4ada5ab44165139b0ad26e9b7b54faaadf28ce9e
Steve Greenberg <spgreenberg@gmail.com>
2015-01-06 10:09:02 -0700

    automate vsphere networking setup

---
+++ set-up/vsphere/networking.sh
@@ -0,0 +1,11 @@
+# Remove the default VM Network portgroup from vSwitch0
+esxcli -s $VSPHERE_SERVER -u $VSPHERE_USER -p $VSPHERE_PWD network vswitch standard portgroup add --portgroup-name='VM Network' --vswitch-name=vSwitch0
+
+# Add a WAN portgroup to vSwitch0
+esxcli -s $VSPHERE_SERVER -u $VSPHERE_USER -p $VSPHERE_PWD network vswitch standard portgroup add --portgroup-name=WAN --vswitch-name=vSwitch0
+
+# Add a new vSwitch for the LAN - vSwitch1
+esxcli -s $VSPHERE_SERVER -u $VSPHERE_USER -p $VSPHERE_PWD network vswitch standard add --vswitch-name=vSwitch1
+
+# Add a LAN portgroup to vSwitch1
+esxcli -s $VSPHERE_SERVER -u $VSPHERE_USER -p $VSPHERE_PWD network vswitch standard portgroup add --portgroup-name=LAN --vswitch-name=vSwitch1
