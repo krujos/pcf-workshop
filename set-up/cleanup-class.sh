6fa4cd155ab72d04f9b17258be6048f52b25e462
krujos <jkruck@pivotal.io>
2015-04-30 23:30:50 -0500

    Scripts to setup a class

---
+++ set-up/cleanup-class.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for email in `cat class-participant-emails.txt` ; do
+
+	org=`echo $email | tr '_' "\n" | colrm 2 | tr -d "\n" && echo -org`
+	echo "INFO: Cleaning up $org: for $email"
+
+	cf delete-org $org -f 
+	cf delete-user $email -f
+
+done
---
+++ set-up/setup-class.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cf create-security-group development-security-group ./development-security-group.json
+
+for email in `cat class-participant-emails.txt` ; do
+
+	org=`echo $email | tr '_' "\n" | colrm 2 | tr -d "\n" && echo -org`
+	echo "INFO: Setting up $org: for $email"
+
+	cf create-org $org
+	cf create-space development -o $org
+	cf create-space production -o $org
+	cf create-user $email password
+	
+	cf set-org-role admin $org OrgManager
+	cf set-org-role $email $org OrgManager
+	cf set-space-role $email $org development SpaceManager
+	cf set-space-role $email $org development SpaceDeveloper
+	cf set-space-role $email $org production SpaceManager
+	cf set-space-role $email $org production SpaceDeveloper
+	cf bind-security-group development-security-group $org development
+
+done
